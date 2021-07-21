cb721aa3847133deecbb9f20f11e5cc754f5f9aa
Heranca <heranca.kamalo@gmail.com>
2021-07-21 14:47:37 +0200

    display messages test ok

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,19 +1,37 @@
+/* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 import { React, useEffect, useState } from 'react';
 import socketIOClient from 'socket.io-client';
 
 export default function Chat() {
-  const [messages, setMessages] = useState('');
+  const [receivedMessages, setReceivedMessages] = useState('');
+  const [socketServer, setsocketServer] = useState();
 
   useEffect(() => {
     const socket = socketIOClient('http://localhost:3001');
-    setMessages(socket);
+    setsocketServer(socket);
+    // get the message list
   }, []);
 
+  if (socketServer) {
+    socketServer.on('initialMessageList', (messages) => {
+      console.log(messages);
+      setReceivedMessages(messages);
+    });
+  }
   return (
     <>
       <div className="instant-message">
-        <div className="messages">messages</div>
+        <div className="messages">
+          <ul>
+            {receivedMessages &&
+              receivedMessages.map((message) => (
+                <li>
+                  {message.author} : {message.text}
+                </li>
+              ))}
+          </ul>
+        </div>
         <input type="message" />
         <button type="submit">send</button>
       </div>
