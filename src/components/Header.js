cd1049520befa070768b631cf962b446e268447e
Heranca <heranca.kamalo@gmail.com>
2021-07-22 14:09:07 +0200

    add app bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,51 @@
 import { NavLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    position: 'fixed',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 export default function Header() {
+  const classes = useStyles();
+
   return (
     <>
-      <header>
-        <nav className="navBar">
-          <ul className="navList">
-            <li className="navItem">
-              <NavLink className="navLink" exact to="/login">
-                Login
-              </NavLink>
-            </li>
-            <li className="navItem">
-              <NavLink className="navLink" exact to="/home">
-                Home
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <AppBar className={classes.root} position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Profil Page
+          </Typography>
+          <NavLink className="navLink" exact to="/home">
+            <Button color="inherit">Home</Button>
+          </NavLink>
+          <NavLink className="navLink" exact to="/login">
+            <Button color="inherit">Login</Button>
+          </NavLink>
+        </Toolbar>
+      </AppBar>
     </>
   );
 }
